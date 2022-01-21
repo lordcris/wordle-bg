18c4d3d18746a6c9bf696e848129c8c4d1531eec
lordcris <lordcris@yahoo.it>
2022-01-21 22:51:22 +0200

    a

--- wordle-it.js
+++ wordle-it.js
@@ -11424,7 +11424,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function (e) {
                                 r = e.isWin,
                                 n = JSON.parse(window.localStorage.getItem(j)),
                                 i = JSON.parse(window.localStorage.getItem(S)),
-                                l = "Par🇮🇹le ".concat(s);
+                                l = "УЪРДЛИ 🇧🇬 ".concat(s);
                             l += " ".concat(r ? t : "X", "/").concat(6), o && (l += "*");
                             var d = "";
                             return a.forEach((function (e) {
